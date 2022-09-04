d37e942fce424a384632a313368035867a489f17
Tona <tona@mango-byte.com>
2022-09-04 10:45:07 +0700

    remove exception unuse

--- src/main/java/com/mb/article/exceptions/ApiError.java
+++
@@ -1,4 +0,0 @@
-package com.mb.article.exceptions;
-
-@Deprecated
-public record ApiError(Boolean success, String message) {}
--- src/main/java/com/mb/article/exceptions/ApiValidationError.java
+++
@@ -1,6 +0,0 @@
-package com.mb.article.exceptions;
-
-import java.util.Map;
-
-@Deprecated
-public record ApiValidationError(Boolean success, String message, Map<String, String> errors) {}
