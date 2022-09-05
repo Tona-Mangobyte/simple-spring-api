7814ac2bef7fd271375ef2bb28883051e2495c22
Tona <tona@mango-byte.com>
2022-09-05 23:13:18 +0700

    config EntityScan

--- src/main/java/com/mb/article/configure/AppConfigure.java
+++ src/main/java/com/mb/article/configure/AppConfigure.java
@@ -1,6 +1,7 @@
 package com.mb.article.configure;
 
 import com.mb.article.security.AuditorAwareImpl;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories("com.mb.article.repositories")
+@EntityScan("com.mb.article.models")
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class AppConfigure {
 
